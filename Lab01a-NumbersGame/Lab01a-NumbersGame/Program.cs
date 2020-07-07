df6a2325e7f76b13146c70e034e879f4164c3819
Diane Stephani <diane.stephani@gmail.com>
2020-07-06 21:39:32 -0700

    Did some things, might've worked.

---
+++ Lab01a-NumbersGame/Lab01a-NumbersGame/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab01a_NumbersGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            try
+            {
+                StartSequence();
+            }
+
+            catch
+            {
+                Console.WriteLine("Oops, that didn't work!");
+            }
+
+            finally
+            {
+                Console.WriteLine("All done!");
+            }
+            
+        }
+
+        static void StartSequence()
+        {
+            Console.WriteLine("Please enter a number greater than 0.");
+
+            try
+            {
+                //from https://www.tutorialspoint.com/convert-toint32-method-in-chash
+                double doubleNum = 11.32;
+                int intNum;
+                intNum = Convert.ToInt32(doubleNum);
+                Console.WriteLine("Converted {doubleNum} to {intNum}");
+            }
+
+            catch
+            {
+                Console.WriteLine("Sorry, something went wrong.");
+            }
+        }
+
+    }
+}
---
+++ Prework Code Challenges/Prework Code Challenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prework_Code_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
